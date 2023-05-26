8a0178a441412db6d8f7a8d73aad550d7b567b48
Kris3XIQ <Kristoffer.markow@gmail.com>
2023-05-27 00:03:40 +0200

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,31 @@
-'use strict';
-
 const modeDarkLight = () => {
     const body = document.querySelector('body');
-    const modeChanger = document.getElementById('theme-toggle');
-    const userPreferenceDark = window.matchMedia && window.matchMedia('prefer-color-scheme: dark').matches;
-    const userPreferenceLight = window.matchMedia && window.matchMedia('preference-color-scheme: light').matches;
+    const modeChanger = document.getElementById('theme-toggle-container');
+    const userPreferenceDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userPreferenceLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
 
-    if (userPreferenceDark) {
+    if(userPreferenceDark){
         console.log("User prefer dark mode");
+        console.log(modeChanger);
         body.classList = 'dark';
     }
-    if (userPreferenceLight) {
+    if(userPreferenceLight){
         console.log("User prefer light mode");
         body.classList = 'light';
     }
     
     modeChanger.addEventListener('click', (e) => {
-        console.log(body.classList);
+        console.log("modeChanger");
         changeTheme();
     })
 
     const changeTheme = () => {
         if (body.classList.contains('dark')) {
+            console.log("dark");
             body.classList.toggle('dark');
             body.classList.toggle('light');
         } else if (body.classList.contains('light')) {
+            console.log("light");
             body.classList.toggle('light');
             body.classList.toggle('dark');
         }
