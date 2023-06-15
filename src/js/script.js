c9cdf5c7b35a08c97bcce502773955477dc67b16
Kris3XIQ <Kristoffer.markow@gmail.com>
2023-06-15 18:01:27 +0200

    migrating from desktop

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,28 @@
-const modeDarkLight = () => {
+const modePreference = () => {
     const body = document.querySelector('body');
-    const modeChanger = document.getElementById('theme-toggle-container');
-    const userPreferenceDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const userPreferenceLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
-
-    if(userPreferenceDark){
-        console.log("User prefer dark mode");
-        console.log(modeChanger);
-        body.classList = 'dark';
+    const themeToggler = document.getElementById('theme-toggle-container');
+    const userPreferenceDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userPreferenceLightMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+    if(userPreferenceDarkMode){
+        console.log("User prefers a dark interface");
+        body.classList = "dark";
     }
-    if(userPreferenceLight){
-        console.log("User prefer light mode");
-        body.classList = 'light';
+    if(userPreferenceLightMode){
+        console.log("User prefers a light interface");
+        body.classList = "light";
     }
-    
-    modeChanger.addEventListener('click', (e) => {
-        console.log("modeChanger");
-        changeTheme();
+    themeToggler.addEventListener('click', (e) => {
+        toggleTheme();
     })
-
-    const changeTheme = () => {
-        if (body.classList.contains('dark')) {
-            console.log("dark");
-            body.classList.toggle('dark');
-            body.classList.toggle('light');
-        } else if (body.classList.contains('light')) {
-            console.log("light");
-            body.classList.toggle('light');
-            body.classList.toggle('dark');
+    const toggleTheme = () => {
+        if (body.classList.contains("dark")) {
+            body.classList.toggle("dark");
+            body.classList.toggle("light");
+        } else if (body.classList.contains("light")) {
+            body.classList.toggle("light");
+            body.classList.toggle("dark");
         }
     }
 }
 
-modeDarkLight();
+modePreference();
